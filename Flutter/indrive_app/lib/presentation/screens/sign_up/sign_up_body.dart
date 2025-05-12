4f6f10d8f098d041ba0eb23a327b01265efa7a66
Chris <mavroboticswebdev7690@gmail.com>
2025-05-12 17:31:52 -0500

    Update Sign Up

--- Flutter/indrive_app/lib/presentation/screens/sign_up/sign_up_body.dart
+++ Flutter/indrive_app/lib/presentation/screens/sign_up/sign_up_body.dart
@@ -33,151 +33,187 @@ class SignUpBody extends StatelessWidget {
               height: double.infinity,
             ),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(30),
-              bottomLeft: Radius.circular(30),
-            ),
-            child: Image.asset(
-              'assets/img/city_background.jpg',
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: 140,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 26, vertical: 16),
-            child: Column(
-              children: [
-                SizedBox(height: 120),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.76,
-                  padding: EdgeInsets.all(20),
+          ListView(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+                child: Image.asset(
+                  'assets/img/city_background.jpg',
+                  fit: BoxFit.cover,
                   width: double.infinity,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(22),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromRGBO(128, 128, 128, 1),
-                          blurRadius: 8,
-                          spreadRadius: 8,
+                  height: 140,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 26, vertical: 16),
+                child: Column(
+                  children: [
+                    // SizedBox(height: 100),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.76,
+                      padding: EdgeInsets.all(20),
+                      width: double.infinity,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(22),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(128, 128, 128, 1),
+                              blurRadius: 8,
+                              spreadRadius: 8,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Lottie.asset(
-                          'assets/gifs_lottie/sign_in.json',
-                          width: 180,
-                          repeat: true,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Nombre',
-                          icon: Icons.person_outline,
-                          margin: EdgeInsets.only(right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              NameChangedEvent(
-                                name: ValidateDataBloc(value: text),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Lottie.asset(
+                                'assets/gifs_lottie/sign_in.json',
+                                width: 180,
+                                repeat: true,
                               ),
-                            );
-                          },
-                          validator: (value) => signUpBlocState.name.error,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Apellido',
-                          icon: Icons.person_2_outlined,
-                          margin: EdgeInsets.only(top: 22, right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              LastnameChangedEvent(
-                                lastname: ValidateDataBloc(value: text),
+                              TextFormFieldWidget(
+                                text: 'Nombre',
+                                icon: Icons.person_outline,
+                                margin: EdgeInsets.only(right: 22, left: 22),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    NameChangedEvent(
+                                      name: ValidateDataBloc(value: text),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) => signUpBlocState.name.error,
                               ),
-                            );
-                          },
-                          validator: (value) => signUpBlocState.lastname.error,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Email',
-                          icon: Icons.email_outlined,
-                          margin: EdgeInsets.only(top: 22, right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              EmailChangedEvent(
-                                email: ValidateDataBloc(value: text),
+                              TextFormFieldWidget(
+                                text: 'Apellido',
+                                icon: Icons.person_2_outlined,
+                                margin: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                ),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    LastnameChangedEvent(
+                                      lastname: ValidateDataBloc(value: text),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) => signUpBlocState.lastname.error,
                               ),
-                            );
-                          },
-                          validator: (value) => signUpBlocState.email.error,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Telefono',
-                          icon: Icons.phone_android_outlined,
-                          margin: EdgeInsets.only(top: 22, right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              PhoneChangedEvent(
-                                phone: ValidateDataBloc(value: text),
+                              TextFormFieldWidget(
+                                text: 'Email',
+                                icon: Icons.email_outlined,
+                                margin: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                ),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    EmailChangedEvent(
+                                      email: ValidateDataBloc(value: text),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) => signUpBlocState.email.error,
                               ),
-                            );
-                          },
-                          validator: (value) => signUpBlocState.phone.error,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Contraseña',
-                          icon: Icons.lock_outline,
-                          margin: EdgeInsets.only(top: 22, right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              PasswordChangedEvent(
-                                password: ValidateDataBloc(value: text),
+                              TextFormFieldWidget(
+                                text: 'Telefono',
+                                icon: Icons.phone_android_outlined,
+                                margin: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                ),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    PhoneChangedEvent(
+                                      phone: ValidateDataBloc(value: text),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) => signUpBlocState.phone.error,
                               ),
-                            );
-                          },
-                          validator: (value) => signUpBlocState.password.error,
-                        ),
-                        TextFormFieldWidget(
-                          text: 'Confirmar Contraseña',
-                          icon: Icons.lock_outline,
-                          margin: EdgeInsets.only(top: 22, right: 22, left: 22),
-                          backdrop: Color.fromRGBO(224, 224, 224, 1),
-                          onChange: (text) {
-                            context.read<SignUpBloc>().add(
-                              ConfirmPasswordChangedEvent(
-                                confirmPassword: ValidateDataBloc(value: text),
+                              TextFormFieldWidget(
+                                text: 'Contraseña',
+                                icon: Icons.lock_outline,
+                                margin: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                ),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    PasswordChangedEvent(
+                                      password: ValidateDataBloc(value: text),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) => signUpBlocState.password.error,
                               ),
-                            );
-                          },
-                          validator:
-                              (value) => signUpBlocState.confirmPassword.error,
-                        ),
-                        ButtonWidget(
-                          text: 'Sign Up',
-                          onPush: () {
-                            context.read<SignUpBloc>().add(FormSubmitEvent());
-                          },
-                          backdropColor: Colors.orange,
-                          textColor: Colors.white,
-                          fringe: EdgeInsets.only(
-                            top: 22,
-                            right: 22,
-                            left: 22,
-                            bottom: 22,
+                              TextFormFieldWidget(
+                                text: 'Confirmar Contraseña',
+                                icon: Icons.lock_outline,
+                                margin: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                ),
+                                backdrop: Color.fromRGBO(224, 224, 224, 1),
+                                onChange: (text) {
+                                  context.read<SignUpBloc>().add(
+                                    ConfirmPasswordChangedEvent(
+                                      confirmPassword: ValidateDataBloc(
+                                        value: text,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                validator:
+                                    (value) =>
+                                        signUpBlocState.confirmPassword.error,
+                              ),
+                              ButtonWidget(
+                                text: 'Sign Up',
+                                onPush: () {
+                                  context.read<SignUpBloc>().add(
+                                    FormSubmitEvent(),
+                                  );
+                                },
+                                backdropColor: Colors.orange,
+                                textColor: Colors.white,
+                                fringe: EdgeInsets.only(
+                                  top: 22,
+                                  right: 22,
+                                  left: 22,
+                                  bottom: 22,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
