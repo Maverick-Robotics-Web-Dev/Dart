4d11969e11cfc16c2f459f555f30921db5cab1a5
Chris <mavroboticswebdev7690@gmail.com>
2025-06-19 17:29:06 -0500

    Add file templates

--- Flutter/ecommerce_app/lib/features/data/data_sources/remote/user/user_remote_data_source.dart
+++ Flutter/ecommerce_app/lib/features/data/data_sources/remote/user/user_remote_data_source.dart
@@ -11,8 +11,8 @@ import 'package:ecommerce_app/features/domain/entities/users/user.dart';
 class UserRemoteDataSource {
   SharedPref sharedPref;
   final _dio = Dio();
-  // final String _urlUpdate = '${ApiConfig.apiEcommercePath}/users/';
-  final String _urlUpdate = '${ApiConfig.apiEcommerceHomePath}/users/';
+  final String _urlUpdate = '${ApiConfig.apiEcommercePath}/users/';
+  // final String _urlUpdate = '${ApiConfig.apiEcommerceHomePath}/users/';
 
   UserRemoteDataSource(this.sharedPref);
 
@@ -46,10 +46,8 @@ class UserRemoteDataSource {
       String token = '';
       MultipartFile imageFile = await multiFilePart(file: user.image);
       Map<String, dynamic> userJson = UserModel.fromEntity(user).toJson();
-      print('IMAGE: ${imageFile}');
-      print('JSON: ${userJson}');
+
       userJson['image'] = imageFile;
-      print('JSONUPDATE: ${userJson}');
       FormData userFormData = FormData.fromMap(userJson);
       final data = await sharedPref.read('user');
 
@@ -57,7 +55,7 @@ class UserRemoteDataSource {
         SignInResponseModel signInResponse = SignInResponseModel.fromJson(data);
         token = signInResponse.token;
       }
-      print('Data: ${userFormData.fields}');
+
       Response<dynamic> response = await _dio.patch(
         urlUpdate,
         data: userFormData,
@@ -73,12 +71,10 @@ class UserRemoteDataSource {
       return userResponse;
     } catch (e) {
       if (e is DioException) {
-        print('DioError: ${e.response}');
         throw DioServerError(
           message: getMessageString(e.response?.data["msg"]),
         );
       } else {
-        print('Error: $e');
         throw ServerError(message: e.toString());
       }
     }
