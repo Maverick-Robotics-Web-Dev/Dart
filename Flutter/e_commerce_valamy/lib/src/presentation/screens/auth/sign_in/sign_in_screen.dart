bff0a27e9b75e8b148800fad5b1349d3fa3bd25b
Maverick <mavroboticswebdev7690@gmail.com>
2025-07-01 00:01:03 -0500

    Here we go

--- Flutter/e_commerce_valamy/lib/src/presentation/screens/auth/sign_in/sign_in_screen.dart
+++ Flutter/e_commerce_valamy/lib/src/presentation/screens/auth/sign_in/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_valamy/config/constants.dart';
+import 'package:e_commerce_valamy/src/presentation/widgets/txt_form_field_custom.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -9,37 +10,55 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset('assets/images/login_img.jpg', fit: BoxFit.cover),
-              Padding(
-                padding: EdgeInsets.all(defaultPadding),
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Peleteria Valamy',
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    SizedBox(height: defaultPadding / 2),
-                    Text(
-                      // "Log in with your data that you intered during your registration.",
-                      'Inicia sesión con tus datos que ingresaste durante tu registro',
-                    ),
-                    SizedBox(height: defaultPadding),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: Column(children: [_headerScreen(), _headerTexts(context)]),
         ),
       ),
     );
   }
+
+  Widget _headerScreen() =>
+      Image.asset('assets/images/login_img.jpg', fit: BoxFit.cover);
+
+  Widget _headerTexts(BuildContext context) => Padding(
+    padding: EdgeInsets.all(defaultPadding),
+    child: Column(
+      // mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Peleteria Valamy',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        SizedBox(height: defaultPadding / 2),
+        Text(
+          // "Log in with your data that you intered during your registration.",
+          'Inicia sesión con tus datos que ingresaste durante tu registro',
+        ),
+        SizedBox(height: defaultPadding),
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TxtFormFieldCustom(
+                svgPath: "assets/icons/Message.svg",
+                hintText: "Email address",
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TxtFormFieldCustom(
+                svgPath: "assets/icons/Lock.svg",
+                hintText: "Password",
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
