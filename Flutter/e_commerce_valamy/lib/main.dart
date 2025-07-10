3990acc2ba38b928d05858512d2d5acbdcdbd5a8
Chris <mavroboticswebdev7690@gmail.com>
2025-07-10 17:24:02 -0500

    Responsive

--- Flutter/e_commerce_valamy/lib/main.dart
+++ Flutter/e_commerce_valamy/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:e_commerce_valamy/config/routes/path_routes.dart';
 import 'package:e_commerce_valamy/config/routes/routes.dart';
 import 'package:e_commerce_valamy/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,6 +14,16 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder:
+          (context, widget) => ResponsiveBreakpoints(
+            breakpoints: [
+              const Breakpoint(start: 0, end: 450, name: MOBILE),
+              const Breakpoint(start: 451, end: 800, name: TABLET),
+              const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+              const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+            ],
+            child: ClampingScrollWrapper.builder(context, widget!),
+          ),
       debugShowCheckedModeBanner: false,
       title: 'Valamy',
       theme: lightTheme(context),
