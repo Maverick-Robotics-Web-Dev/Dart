65969b4203c7a9b8ae2714334d68a00b1759113a
Chris <mavroboticswebdev7690@gmail.com>
2025-07-28 18:00:16 -0500

    Now

--- Flutter/e_commerce_valamy/lib/src/presentation/screens/auth/sign_in/sign_in_tablet_screen.dart
+++ Flutter/e_commerce_valamy/lib/src/presentation/screens/auth/sign_in/sign_in_tablet_screen.dart
@@ -1,7 +1,6 @@
 import 'package:e_commerce_valamy/config/constants.dart';
 import 'package:e_commerce_valamy/src/presentation/widgets/txt_form_field_custom.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SignInTabletScreen extends StatefulWidget {
   final double height;
@@ -46,134 +45,138 @@ class _SignInTabletScreenState extends State<SignInTabletScreen> {
                 ),
               ),
             ),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(13, 71, 161, 0.4),
+              ),
+            ),
             Row(
               children: [
-                Expanded(
-                  // flex: 2,
-                  child: Container(
-                    // height: MediaQuery.of(context).size.height,
-                    width: double.infinity,
-                    margin: EdgeInsets.all(76),
-                    padding: EdgeInsets.symmetric(horizontal: 80),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFFAFAFA),
-                      // color: Color.fromRGBO(8, 41, 108, 0.2),
-                      // color: Color.fromRGBO(255, 255, 255, 1),
-                      // border: Border.all(color: primaryColor),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        // Text('Sign In', style: textTheme?.headlineLarge),
-                        Text(
+                Container(
+                  // height: MediaQuery.of(context).size.height,
+                  width: widget.width * 0.6,
+                  // margin: EdgeInsets.all(76),
+                  padding: EdgeInsets.symmetric(horizontal: 140),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFAFAFA),
+                    // color: Color.fromRGBO(8, 41, 108, 0.2),
+                    // color: Color.fromRGBO(255, 255, 255, 1),
+                    // border: Border.all(color: primaryColor),
+                    // borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Text('Sign In', style: textTheme?.headlineLarge),
+                      Text(
+                        'Sign In',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: primaryColor,
+                          fontSize: 34,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                      SizedBox(height: 26),
+                      Text(
+                        // "Log in with your data that you intered during your registration.",
+                        'Inicia sesión con tus datos que ingresaste durante tu registro',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: secondaryColor,
+                          fontSize: 16,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Form(
+                        key: widget.formKey,
+                        child: Column(
+                          children: [
+                            TxtFormFieldCustom(
+                              svgPath: "assets/icons/Message.svg",
+                              hintText: "Email",
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            SizedBox(height: 40),
+                            TxtFormFieldCustom(
+                              svgPath: "assets/icons/Lock.svg",
+                              hintText: "Password",
+                              obscureText: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 14),
+                      Align(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '¿Olvidaste tu contraseña?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: primaryColor,
+                              fontSize: 18,
+                              letterSpacing: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height:
+                            widget.height > 700
+                                ? (widget.height * 0.1)
+                                : (h_16),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 32),
+                          backgroundColor: primaryColor,
+                        ),
+                        child: Text(
                           'Sign In',
-                          textAlign: TextAlign.center,
                           style: TextStyle(
-                            color: primaryColor,
-                            fontSize: 34,
+                            fontSize: 18,
+                            // fontWeight: FontWeight.bold,
                             letterSpacing: 1,
+                            color: whiteColor,
                           ),
                         ),
-                        SizedBox(height: 26),
-                        Text(
-                          // "Log in with your data that you intered during your registration.",
-                          'Inicia sesión con tus datos que ingresaste durante tu registro',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: secondaryColor,
-                            fontSize: 16,
-                            letterSpacing: 1,
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '¿No tienes una cuenta?',
+                            style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 18,
+                              // fontWeight: FontWeight.w500,
+                              letterSpacing: 1,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 40),
-                        Form(
-                          key: widget.formKey,
-                          child: Column(
-                            children: [
-                              TxtFormFieldCustom(
-                                svgPath: "assets/icons/Message.svg",
-                                hintText: "Email",
-                                keyboardType: TextInputType.emailAddress,
-                              ),
-                              SizedBox(height: 40),
-                              TxtFormFieldCustom(
-                                svgPath: "assets/icons/Lock.svg",
-                                hintText: "Password",
-                                obscureText: true,
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 14),
-                        Align(
-                          child: TextButton(
+                          TextButton(
                             onPressed: () {},
                             child: Text(
-                              '¿Olvidaste tu contraseña?',
-                              textAlign: TextAlign.center,
+                              'Sign Up',
                               style: TextStyle(
                                 color: primaryColor,
-                                fontSize: 18,
-                                letterSpacing: 1,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height:
-                              widget.height > 700
-                                  ? (widget.height * 0.1)
-                                  : (h_16),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size(double.infinity, 32),
-                            backgroundColor: primaryColor,
-                          ),
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(
-                              fontSize: 18,
-                              // fontWeight: FontWeight.bold,
-                              letterSpacing: 1,
-                              color: whiteColor,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              '¿No tienes una cuenta?',
-                              style: TextStyle(
-                                color: secondaryColor,
                                 fontSize: 18,
                                 // fontWeight: FontWeight.w500,
                                 letterSpacing: 1,
                               ),
                             ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text(
-                                'Sign Up',
-                                style: TextStyle(
-                                  color: primaryColor,
-                                  fontSize: 18,
-                                  // fontWeight: FontWeight.w500,
-                                  letterSpacing: 1,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
+                Container(
+                  width: widget.width * 0.4,
                   child: Stack(
                     alignment: Alignment.center,
                     children: [
@@ -208,7 +211,7 @@ class _SignInTabletScreenState extends State<SignInTabletScreen> {
                         // crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
                           Image.asset(
-                            'assets/images/logo_valamy_azul_m.png',
+                            'assets/images/logo_valamy_blanco.png',
                             width: 260,
                             height: 260,
                           ),
@@ -224,7 +227,7 @@ class _SignInTabletScreenState extends State<SignInTabletScreen> {
                           Text(
                             'Peleteria Valamy',
                             style: TextStyle(
-                              color: primaryColor,
+                              color: Colors.white,
                               fontSize: 48,
                               letterSpacing: 1,
                               // fontWeight: FontWeight.w500,
@@ -233,7 +236,7 @@ class _SignInTabletScreenState extends State<SignInTabletScreen> {
                           Text(
                             'Exclusividad y Sofisticación',
                             style: TextStyle(
-                              color: secondaryColor,
+                              color: Colors.white,
                               fontSize: 22,
                               letterSpacing: 1,
                             ),
