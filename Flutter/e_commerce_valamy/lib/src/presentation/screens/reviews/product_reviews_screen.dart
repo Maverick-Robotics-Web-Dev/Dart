619ff77945f11bc69dee1313a00f0b0e55d22c4b
Chris <mavroboticswebdev7690@gmail.com>
2025-11-24 17:47:32 -0500

    Rerview Card

--- Flutter/e_commerce_valamy/lib/src/presentation/screens/reviews/product_reviews_screen.dart
+++ Flutter/e_commerce_valamy/lib/src/presentation/screens/reviews/product_reviews_screen.dart
@@ -1,11 +1,56 @@
 import 'package:e_commerce_valamy/config/constants.dart';
 import 'package:e_commerce_valamy/src/presentation/widgets/review_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/svg.dart';
 
-List<DropdownMenuItem> items = [
-  DropdownMenuItem(value: "most", label: "Most useful"),
-  DropdownMenuItem(value: "recent", label: "Recent"),
+class MenuItemModel {
+  final String value;
+  final String label;
+
+  MenuItemModel({required this.value, required this.label});
+}
+
+class ReviewCardModel {
+  final String imageUrl;
+  final String userName;
+  final String publishTime;
+  final double rating;
+  final String description;
+
+  ReviewCardModel({
+    required this.imageUrl,
+    required this.userName,
+    required this.rating,
+    required this.description,
+    required this.publishTime,
+  });
+}
+
+List<MenuItemModel> items = [
+  MenuItemModel(value: "most", label: "Most useful"),
+  MenuItemModel(value: "recent", label: "Recent"),
+];
+
+List<ReviewCardModel> reviewCards = [
+  ReviewCardModel(
+    imageUrl: 'assets/images/no_image_user.png',
+    userName: 'Andrew',
+    publishTime: '36s ago',
+    rating: 3,
+    description:
+        'Molestiae id magnam odit rerumvoluptatem aliquam laudantium hic. Sunt et ut delectus quaerat incidunt in molestiae accusantium. Quia officia doloribus. Adipisci consequatur est deserunt sit aut quasi eum. Molestiae aspernatur qui consequatur dolores.',
+  ),
+  ReviewCardModel(
+    imageUrl: 'assets/images/scarlett_johansson.jpg',
+    userName: 'Scarlet',
+    publishTime: '36s ago',
+    rating: 5,
+    description: """Molestiae id magnam odit rerumvoluptatem
+        aliquam laudantium hic. Sunt et ut delectus
+        quaerat incidunt in molestiae accusantium. Quia officia doloribus. Adipisci consequatur est deserunt sit aut quasi eum.
+        Molestiae aspernatur qui consequatur dolores.""",
+  ),
 ];
 
 class ProductReviewsScreen extends StatefulWidget {
@@ -16,7 +61,7 @@ class ProductReviewsScreen extends StatefulWidget {
 }
 
 class _ProductReviewsScreenState extends State<ProductReviewsScreen> {
-  String selected = "most";
+  String? selected = "most";
 
   @override
   Widget build(BuildContext context) {
@@ -80,23 +125,161 @@ class _ProductReviewsScreenState extends State<ProductReviewsScreen> {
               ],
             ),
           ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-              child: Row(
-                children: [
-                  Text(
-                    'User Reviews',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                  Spacer(),
-                  DropdownButton(items: items, onChanged: onChanged),
-                ],
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: _HeaderDelegate(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Text(
+                      'User Reviews',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Spacer(),
+                    DropdownButton(
+                      value: selected,
+                      icon: SvgPicture.asset(
+                        'assets/icons/miniDown.svg',
+                        height: 30,
+                        color: Theme.of(context).textTheme.bodyLarge!.color,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          selected = value;
+                        });
+                      },
+                      selectedItemBuilder:
+                          (context) =>
+                              items.map((item) {
+                                return Row(
+                                  children: [
+                                    SvgPicture.asset(
+                                      'assets/icons/Sort.svg',
+                                      height: 29,
+                                      color:
+                                          Theme.of(
+                                            context,
+                                          ).textTheme.bodyLarge!.color,
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      item.label,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }).toList(),
+                      items:
+                          items.map((item) {
+                            return DropdownMenuItem(
+                              value: item.value,
+                              child: Text(item.label),
+                            );
+                          }).toList(),
+                    ),
+                  ],
+                ),
               ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Theme.of(
+                      context,
+                    ).textTheme.bodyLarge!.color!.withOpacity(0.035),
+                    borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 16,
+                                backgroundImage: AssetImage(
+                                  reviewCards[index].imageUrl,
+                                ),
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                reviewCards[index].userName,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                reviewCards[index].publishTime,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                          RatingBar.builder(
+                            initialRating: reviewCards[index].rating,
+                            itemSize: 18,
+                            itemPadding: const EdgeInsets.only(right: 16 / 4),
+                            unratedColor: Theme.of(
+                              context,
+                            ).textTheme.bodyLarge!.color!.withOpacity(0.08),
+                            glow: false,
+                            allowHalfRating: true,
+                            ignoreGestures: true,
+                            onRatingUpdate: (value) {},
+                            itemBuilder:
+                                (context, index) => SvgPicture.asset(
+                                  "assets/icons/Star_filled.svg",
+                                ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              childCount: reviewCards.length,
             ),
           ),
         ],
       ),
     );
   }
+}
+
+class _HeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+
+  _HeaderDelegate({required this.child});
+
+  @override
+  Widget build(context, double shrinkOffset, bool overlapsContent) {
+    return child;
+  }
+
+  @override
+  double get maxExtent => 60; // altura de la Row
+  @override
+  double get minExtent => 60;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      true;
 }
--- Flutter/e_commerce_valamy/lib/src/presentation/widgets/rate_bar.dart
+++ Flutter/e_commerce_valamy/lib/src/presentation/widgets/rate_bar.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce_valamy/config/constants.dart';
 import 'package:flutter/material.dart';
 
 class RateBar extends StatelessWidget {
